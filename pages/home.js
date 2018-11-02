8a69f07899a4569c2535f8daff0a2b01e00b60ce
tomibudis <tomibudis@gmail.com>
2018-11-02 16:04:27 +0700

    nextjs for unit test

---
+++ pages/home.js
@@ -0,0 +1,5 @@
+export default  () => {
+    return(
+        <h1 className="home">Home</h1>
+    )
+}
---
+++ test/home.test.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+
+const WIDTH = 1920
+const HEIGHT = 900
+
+isDebugging = () => {
+    const debuggingMode = {
+      headless : false,
+      slowMo : 150,
+      devtools : true
+      // args: [`--window-size=${WIDTH},${HEIGHT}`]
+    }
+    return process.env.NODE_ENV === 'debug' ? debuggingMode : {}
+ }
+ var browser, page
+ beforeAll( async () => {
+   browser =  await puppeteer.launch( isDebugging() )
+   page = await browser.newPage()
+   await page.on('console', msg => console.log('PAGE LOG:', msg.text() ));
+
+  //  page.setViewport({ width: 1680 , height:900})
+  
+   await page.goto('http://localhost:3000/home')
+   
+ },50000)
+
+let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('NEXT Puppeteer', () => {
+  test('home page test', async() => {
+    await page.waitForSelector('.home')
+    var html = await page.$eval('.home', (element) => element.textContent);
+    expect(html).toBe('Home')
+  },15000)
+  
+})
+
+afterAll( async () => {
+    if(isDebugging()){
+      browser.close()
+    }
+})
